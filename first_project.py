87fcb67a52bca7110c3979306f5c4aaa7220369d
iskandar-ops <iskandarmuhammadjanov@gmail.com>
2026-02-27 10:50:53 +0500

    Create first_project.py

---
+++ first_project.py
@@ -0,0 +1,8 @@
+# first_project.py
+print("Hello, world! 👋")
+
+# Foydalanuvchidan ism va yoshni so‘rash
+name = input("Ismingiz nima? ")
+age = input("Yoshingiz nechida? ")
+
+print(f"Salom, {name}! Siz {age} yoshdasiz.")
